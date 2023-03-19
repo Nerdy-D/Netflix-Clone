ec1800a658c051799c288ec96b9ed121d6e598f4
Nerdy-D <111739562+Nerdy-D@users.noreply.github.com>
2023-03-19 03:31:33 +0200

    I have finally loaded all the movies 03:31

--- components/movie-tile.js
+++ components/movie-tile.js
@@ -1,11 +1,12 @@
 Vue.component('movie-tile', {
-    props: ['title', 'img'],
+    props: ['name', 'image', 'description', 'release_date'],
     template: `
     <div class="movie-tile">
-    <img v-bind:src="img" width="250px" height="350px">
-        <div class="title">
-        {{title}}
-        </div>
-    </div>`
-    ,
+    <img v-bind:src="image" width="200px">
+    <h3 class="name">
+    {{name}}
+    </h3>
+    <p class="description">{{description}}</p>
+    <h4>{{release_date}}</h4>
+    </div>`,
 });
