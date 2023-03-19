8cb6b0217b7dd163fcc51d8bf6eb312efaa747cc
Nerdy-D <111739562+Nerdy-D@users.noreply.github.com>
2023-03-19 17:51:48 +0200

    username displayed in bar

--- netflix.js
+++ netflix.js
@@ -1,33 +1,7 @@
 var app = new Vue({
     el: '#myVueApp',
     data: {
-        // movies: [
-        //     {
-        //         nameOfMovie: 'Die Hard',
-        //         coverArt:
-        //             'https://m.media-amazon.com/images/M/MV5BZjRlNDUxZjAtOGQ4OC00OTNlLTgxNmQtYTBmMDgwZmNmNjkxXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg',
-        //     },
-        //     {
-        //         nameOfMovie: '50 First Dates',
-        //         coverArt:
-        //             'https://www.liveabout.com/thmb/nXVsMr5zc2TdbTF8L8ON2ybuveg=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/50First-56b236035f9b58def9c848bf.jpg',
-        //     },
-        //     {
-        //         nameOfMovie: 'Squid Game',
-        //         coverArt:
-        //             'https://wp.umpi.edu/utimes/files/2022/04/Netflixs-Squid-Game-Cover-Art-691x1024.jpeg',
-        //     },
-        //     {
-        //         nameOfMovie: 'Daddys Home',
-        //         coverArt:
-        //             'https://timesofindia.indiatimes.com/photo/61246288.cms',
-        //     },
-        //     {
-        //         nameOfMovie: 'Italian Job',
-        //         coverArt:
-        //             'https://media-cache.cinematerial.com/p/500x/zpfukcah/the-italian-job-movie-cover.jpg?v=1456317354',
-        //     },
-        // ]
+        userName: "test",
         movies: [
             {
                 id: 1,
@@ -3849,6 +3823,11 @@ var app = new Vue({
             },
         ],
 
+    },
+    created: function () {
+        this.userName = localStorage.getItem("username");
+        // `this` points to the vm instance
+        console.log('username is: ' + this.userName);
     }
 
 });
