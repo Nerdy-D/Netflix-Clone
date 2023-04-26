56a750d4fd82cbad2c9d7d31055704c7ea2475bc
Nerdy-D <111739562+Nerdy-D@users.noreply.github.com>
2023-04-26 13:13:42 +0200

    Completed Resub

--- Components/movietile.js
+++ Components/movietile.js
@@ -1,16 +1,15 @@
 Vue.component('movie-tile', {
-    props: ['id', 'name', 'image', 'description', 'release_date', 'coming_soon', 'genre', 'preview', 'show-add', 'show-remove'],
+    props: ['id', 'name', 'image', 'description', 'released', 'coming_soon', 'genre', 'preview', 'show-add', 'show-remove'],
     template: `
     <div class="movie-tile">
-    <img v-bind:src="image" width="350px" height="350px">
+    <img v-bind:src="image" width="350px" height="450px">
     <h3 class="name">
     {{name}}
     </h3>
     <p class="description">{{description}}</p>
-    <p class="rdate">{{release_date}}</p>
-    <p class="coming_soon">{{coming_soon}}</p>
+    <a><p class="released">{{released}}</p>
     <p class="genre">{{genre}}</p>
-    <p class="id">{{id}}</p>
+    <p class="id">{{id}}</p></a>
     <a v-bind:href="preview"><input type="button" value="Preview"></a>
     <button v-if="showAdd" v-on:click="$emit('add-to-watch-list')">Add</button>
     <button v-if="showRemove" v-on:click="$emit('remove-from-watch-list')">Remove</button>
